883c2eab6a2b30640e694e074e280e0c1efba178
mattwinkler1983 <matt.winkler1983@aol.com>
2025-11-04 13:10:16 -0700

    Delete WEB COURSE 1 - Copy directory

--- WEB COURSE 1 - Copy/python/PYTHON STUFF/Step60MileageConvertor.py
+++
@@ -1,6 +0,0 @@
-print("How many kilometers did you run?")
-kms = input()
-miles= float(kms)/1.60934
-print(f"That is equal to {round(miles, 2)} miles ")
-
-# round(thing to round, how many decimal points)
