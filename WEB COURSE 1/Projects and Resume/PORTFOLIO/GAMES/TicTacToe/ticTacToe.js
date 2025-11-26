c96c595b4e50138aef3679879a2df387c5d346b0
Dave Winkler <matt.winkler1983@aol.com>
2025-11-26 12:58:23 -0700

    corrected tic tac toe

--- WEB COURSE 1/Projects and Resume/PORTFOLIO/GAMES/TicTacToe/ticTacToe.js
+++ WEB COURSE 1/Projects and Resume/PORTFOLIO/GAMES/TicTacToe/ticTacToe.js
@@ -11,17 +11,17 @@ function checkGameBoard() {
     for(let i = 0; i <= 2; i++) {
         checkWinner(document.getElementById(i + "_0").innerText,
             document.getElementById(i + "_1").innerText,
-            document.getElementById(i + "_2").innertext);
+            document.getElementById(i + "_2").innerText);
         checkWinner(document.getElementById("0_" + i).innerText,
             document.getElementById("1_" + i).innerText,
-            document.getElementbyID("2_" + i).innerText);
+            document.getElementById("2_" + i).innerText);
     }
     checkWinner(document.getElementById("0_0").innerText,
-        document.getElementByID("1_1").innerText,
-        document.getElementByID("2_2").innerText);
-    checkWinner(document.getByElementId("0_2").innerText,
+        document.getElementById("1_1").innerText,
+        document.getElementById("2_2").innerText);
+    checkWinner(document.getElementById("0_2").innerText,
     document.getElementById("1_1").innerText,
-    document.getElemebtById("2_0").innerText);
+    document.getElementById("2_0").innerText);
 }
 function checkWinner (first, second, third){
     if ( first != "" && first == second && first == third) {
