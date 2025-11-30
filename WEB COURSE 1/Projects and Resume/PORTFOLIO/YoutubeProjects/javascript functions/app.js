a4e048b31a2d28bf3b8d9482bc2e6300be5e93c9
Dave Winkler <matt.winkler1983@aol.com>
2025-11-30 08:44:47 -0700

    randomNUmber

---
+++ WEB COURSE 1/Projects and Resume/PORTFOLIO/YoutubeProjects/javascript functions/app.js
@@ -0,0 +1,14 @@
+function Daveman() {
+    console.log("it's a bird");
+    console.log("it's a plane");
+    console.log("it's Daveman");
+    alert("Hello Dave.  You are awesome!");
+
+}
+Daveman();
+
+function equation(x, y){
+   alert(x + y);
+   console.log(x + y);
+}
+equation(2, 3);
