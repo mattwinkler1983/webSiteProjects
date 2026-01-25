0850ac92fd4e2a99a99f3c570689e9d2a41355eb
Dave Winkler <matt.winkler1983@aol.com>
2026-01-25 11:35:30 -0700

    random background color

--- WEB COURSE 1/Projects and Resume/PORTFOLIO/JavaScript and HTML small projects/EveryDayProject/everyDayProject.js
+++ WEB COURSE 1/Projects and Resume/PORTFOLIO/JavaScript and HTML small projects/EveryDayProject/everyDayProject.js
@@ -1,4 +1,12 @@
 const button = document.querySelector("#generateBtn");
 const output = document.querySelector("#output");
 
-button.addEventListener("click", funtion() {})
+button.addEventListener("click", function() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    
+    const rgbColor = `rgb(${red}, ${green}, ${blue})`;
+    document.body.style.backgroundColor = rgbColor;
+    output.testConect = "Background Color: " + rgbColor;
+});
