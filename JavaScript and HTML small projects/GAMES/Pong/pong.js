8e93b91564a647a6735d437fc1df07fec7d6f96d
Dave Winkler <matt.winkler1983@aol.com>
2026-03-14 14:02:33 -0700

    pong paddle

--- JavaScript and HTML small projects/GAMES/Pong/pong.js
+++ JavaScript and HTML small projects/GAMES/Pong/pong.js
@@ -1,12 +1,29 @@
+//board
 let board;
 let boardWidth = 500;
 let boardHeight = 500;
 let context;
 
+//players
+let playerWidth = 10;
+let playerHeight = 50;
+
+let player1 = {
+    x : 10,
+    y : boardHeight/2,
+    width : playerWidth,
+    height : playerHeight,
+}
+
 window.onload = function() {
     board = document.querySelector("#board");
     board.height = boardHeight;
     board.width = boardWidth;
-    context = board.getContext("2d");
+    context = board.getContext("2d"); // used to draw on the board
+
+    //draw initial play 1
+    context.fillStyle = "skyblue";
+    context.fillRect(player1.x, player1.y, player1.width, player1.height);
+
 }
     
