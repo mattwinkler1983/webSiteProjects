41ab48af04451cf74338c7eead66c4be5f377dfe
Dave Winkler <matt.winkler1983@aol.com>
2026-03-15 11:53:37 -0700

    updated js

--- JavaScript and HTML small projects/GAMES/Pong/pong.js
+++ JavaScript and HTML small projects/GAMES/Pong/pong.js
@@ -7,18 +7,22 @@ let context;
 //players
 let playerWidth = 10;
 let playerHeight = 50;
+let playerVelocityY = 0; 
 
 let player1 = {
     x : 10,
     y : boardHeight/2,
     width : playerWidth,
     height : playerHeight,
+    velocityY : playerVelocityY,
+    
 }
 let player2 = {
     x : boardWidth - playerWidth - 10,
     y : boardHeight/2,
     width : playerWidth,
     height : playerHeight,
+    velocityY : playerVelocityY,
 }
 
 window.onload = function() {
@@ -27,19 +31,53 @@ window.onload = function() {
     board.width = boardWidth;
     context = board.getContext("2d"); // used to draw on the board
 
-    //draw initial play 1
+    //draw initial player 1
     context.fillStyle = "skyblue";
     context.fillRect(player1.x, player1.y, player1.width, player1.height);
 
-    this.requestAnimationFrame(update);
+    requestAnimationFrame(update);
+    document.addEventListener("keydown", movePlayer);
+    document.addEventListener("keyup", stopPlayer);
 }
     
 function update () {
-    requestAnimationFrame(update); 
+    context.clearRect(0, 0, boardWidth, boardHeight); // clear canvas each frame
+
     //player 1
+    player1.y += player1.velocityY;
     context.fillStyle = "skyblue";
     context.fillRect(player1.x, player1.y, player1.width, player1.height);
+    
+    //player 2
+    player2.y += player2.velocityY;
+    context.fillRect(player2.x, player2.y, player2.width, player2.height );
+
+    requestAnimationFrame(update); 
+}
+
+function movePlayer(e) {
+    //player1 
+    if (e.code === "KeyW") {
+        player1.velocityY = -3;
+    } else if (e.code === "KeyS") {
+        player1.velocityY = 3;
+    }
 
     //player 2
-    context.fillRect(player2.x, player2.y, player2.width, player2.height );
+    if (e.code === "ArrowUp") {
+        player2.velocityY = -3;
+    } else if (e.code === "ArrowDown") {
+        player2.velocityY = 3;
+    }
+}
+
+function stopPlayer(e) {
+    //player1
+    if (e.code === "KeyW" || e.code === "KeyS") {
+        player1.velocityY = 0;
+    }
+    //player2
+    if (e.code === "ArrowUp" || e.code === "ArrowDown") {
+        player2.velocityY = 0;
+    }
 }
